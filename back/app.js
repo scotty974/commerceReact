2c27bfaa7cfd5e88588f2864e80d9c5409cc89a7
scotty974 <fabienethevepro@gmail.com>
2023-12-27 16:39:43 +0100

    ajout de prisma et du schema de la base

--- back/app.js
+++ back/app.js
@@ -5,9 +5,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 const port = 8000
 
-app.get('/',(req,res)=>{
-    res.send("hey")
-})
 
 app.listen(port, ()=>{
     console.log(`App running on : ${port}`)
