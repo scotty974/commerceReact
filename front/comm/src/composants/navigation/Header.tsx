345a7fa227679b9ee0d7459e7593d2fe30aae5b9
scotty974 <fabienethevepro@gmail.com>
2023-12-28 14:23:24 +0100

    ajustement du header

--- front/comm/src/composants/navigation/Header.tsx
+++ front/comm/src/composants/navigation/Header.tsx
@@ -1,13 +1,14 @@
+import { ShoppingCart, CircleUserRound } from "lucide-react";
 export default function Header() {
   return (
     <>
-      <nav className="md:container md:m-auto md:pt-2 pl-2 pr-2 flex items-start md:items-center justify-between">
+      <nav className="md:container md:m-auto md:pt-2 pl-2 md:pl-0 md:pr-0  pr-2 flex items-start md:items-center justify-between">
         <h1 className="text-2xl font-mono">Asphalt</h1>
         <ul className="md:flex">
           <li>
             <a
               href=""
-              className="hover:bg-slate-100 p-2 rounded-full text-neutral-900 font-semibold "
+              className="hover:underline  p-2 rounded-full text-neutral-900 font-semibold "
             >
               Vêtements
             </a>
@@ -15,7 +16,7 @@ export default function Header() {
           <li>
             <a
               href=""
-              className="hover:bg-slate-100 p-2 rounded-full text-neutral-9000 font-semibold "
+              className="hover:underline  p-2 rounded-full text-neutral-9000 font-semibold "
             >
               Nouveautés
             </a>
@@ -23,12 +24,21 @@ export default function Header() {
           <li>
             <a
               href=""
-              className="hover:bg-slate-100 p-2 rounded-full text-neutral-900 font-semibold "
+              className="hover:underline p-2 rounded-full text-neutral-900 font-semibold "
             >
               Contacts
             </a>
           </li>
         </ul>
+        <div className="flex">
+          <button className="mr-4">
+            <ShoppingCart size={24} />
+          </button>
+
+          <button>
+            <CircleUserRound size={24} />
+          </button>
+        </div>
       </nav>
     </>
   );
