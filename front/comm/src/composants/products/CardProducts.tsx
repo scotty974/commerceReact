f2d7b88bf00b2f0dd0ca3be709c875addf55ccf9
scotty974 <fabienethevepro@gmail.com>
2023-12-28 16:21:41 +0100

    ajout des effet animations sur les cards

--- front/comm/src/composants/products/CardProducts.tsx
+++ front/comm/src/composants/products/CardProducts.tsx
@@ -1,6 +1,6 @@
 export default function CardProducts({name, price, category}) {
   return <>
-  <div className=" md:w-1/6 w-1/3 h-full  mb-2">
+  <div className=" md:w-1/6 w-1/3 h-full mb-2 hover:cursor-pointer hover:scale-95 transition ease-in-out duration-300">
   <div className="bg-slate-400 w-full h-60"></div>
   <div >
     <h3>{name}</h3>
