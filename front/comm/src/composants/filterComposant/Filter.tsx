ea487d9b3ac800e5272f8ae304ef57961b997348
scotty974 <fabienethevepro@gmail.com>
2023-12-28 15:03:17 +0100

    fixed filter

--- front/comm/src/composants/filterComposant/Filter.tsx
+++ front/comm/src/composants/filterComposant/Filter.tsx
@@ -4,15 +4,15 @@ import { blue } from "@mui/material/colors";
 export default function Filter() {
   return (
     <>
-      <section className="container m-auto mt-4">
-        <aside className="flex justify-between items-center">
+      <section className="container m-auto mt-4 px-2 md:px-0">
+        <aside className="md:flex justify-between items-center">
           <span className="font-semibold text-neutral-600">
             Search by categories :{" "}
           </span>
-          <ul>
+          <ul className="mt-2">
             <li><Button textColor={blue} fontSize={14}>Pulls</Button></li>
           </ul>
-          <div>
+          <div className="invisible md:visible">
             <Select placeholder="Filter by :">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
