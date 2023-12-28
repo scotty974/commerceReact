edcf7514210206316420a128abd831591a731340
scotty974 <fabienethevepro@gmail.com>
2023-12-28 21:12:12 +0100

    ajout du style de la page product

--- front/comm/src/pages/routeProduct/Product.tsx
+++ front/comm/src/pages/routeProduct/Product.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import Header from "../../composants/navigation/Header";
 type ProductData = {
   id: number;
   name: string;
@@ -7,7 +8,7 @@ type ProductData = {
   // Ajoutez d'autres propriétés au besoin
 };
 
-export async function loader({params}: { params: { id: string } }){
+export async function loader({params}){
   const base_url = "http://localhost:8000/"
   const resp = await fetch(base_url+"products/"+params.id)
   const data:ProductData  = await resp.json()
@@ -19,8 +20,17 @@ export default function Product() {
   const {data} = useLoaderData()
   const { name, description, price } = data;
   return <>
-  <h1>{name}</h1>
+  <Header />
+  <section className="container m-auto flex mt-4">
+  
+    <div className="bg-neutral-900 w-1/2 h-bannerHeight  "></div>
+    <div className="ml-10 w-1/2 flex flex-col justify-center ">
+    <h1 className="text-8xl font-bold ">{name}</h1>
       <p>{description}</p>
       <p>Price: {price}</p>
+    </div>
+      
+  </section>
+  
   </>;
 }
